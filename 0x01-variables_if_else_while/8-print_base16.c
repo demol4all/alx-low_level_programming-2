73b926e4f471b566585bc08aeae74208e13ff317
drewkisambira <107510227+drewkisambira@users.noreply.github.com>
2022-07-03 00:45:52 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,53 +1,27 @@
-i#include <stdio.h>
-
-
+#include <stdio.h>
 
 /**
- *
- * *main -> assign a random number to the variable n each time it is executed
- *
- * *and print the last digit of the number stored in the variable n
- *
- * *Return: Always 0 (Success)
- *
- * */
-
+*main -> assign a random number to the variable n each time it is executed
+*and print the last digit of the number stored in the variable n
+*Return: Always 0 (Success)
+*/
 int main(void)
+{
+char ch;
+int n;
 
+for (n = 48; n <= 57; n++)
 {
 
-	char ch;
+putchar(n);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
 
-	int n;
+putchar(ch);
+}
 
+putchar(10);
 
-
-	for (n = 48; n <= 57; n++)
-
-	{
-
-
-
-		putchar(n);
-
-	}
-
-	for (ch = 'a'; ch <= 'f'; ch++)
-
-	{
-
-
-
-		putchar(ch);
-
-	}
-
-
-
-	putchar(10);
-
-
-
-	return (0);
-
+return (0);
 }

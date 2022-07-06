5d062344470c62eb69e8cbf814741aa8084b8fa8
drewkisambira <107510227+drewkisambira@users.noreply.github.com>
2022-07-07 01:28:22 +0300

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * Description: adds two integers and returns the result
+ * @x: parameter 1
+ * @y: parameter 2
+ * Return: x + y
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}

42e4d6e864e26b7fcfd6d30c20b4ab021e91698f
drewkisambira <107510227+drewkisambira@users.noreply.github.com>
2022-08-04 00:21:33 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,38 +1,37 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
 #include "3-calc.h"
 /**
-* main - checks arguments.
-* @argc: argument count.
-* @argv: argument vector.
-*
-* Return: error if number of arguments is wrong.
-*/
-int main(int argc, char *argv[])
+ *main - Prints the result of simple operations.
+ *@argc: The number of arguments supplied to the program.
+ *@argv: An array of pointers to the arguments.
+ *Return: Always 0.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int a = 0, b = 0, res = 0;
-	char s;
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	/* checks for exclusively one oprator*/
-	if (argv[2][1] != '\0')
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	s = argv[2][0];
-	if (s != '+' && s != '-' && s != '/' && s != '*' && s != '%')
+	if ((*op == '/' && num2 == 0) ||
+			(*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
-
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	res = (get_op_func(argv[2]))(a, b);
-	printf("%d\n", res);
+	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }

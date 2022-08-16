fcdb64f9fdd8aaab8301c1721685185fcc83054f
Kisambira Andrew <andrewkeshaa@gmail.com>
2022-08-16 23:27:47 +0300

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,21 @@
+#include "lists.h"
+/**
+  * listint_len - returns numbr of elements in a
+  * linked listint_t list.
+  * @h: pointer to first element on list.
+  *
+  * Return: number of elements in list
+  */
+size_t listint_len(const listint_t *h)
+{
+	/* i is counter variable */
+	size_t i = 0;
 
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}

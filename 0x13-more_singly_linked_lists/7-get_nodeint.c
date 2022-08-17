7ec44d61fe81bc0a420ec9ca057d4ba4d3e5735b
Kisambira Andrew <andrewkeshaa@gmail.com>
2022-08-17 22:55:21 +0300

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,61 +1,28 @@
 #include "lists.h"
-/**
-  * listint_len - returns number of elements in a listint_t list.
-  * @h: pointer to first element on list.
-  *
-  * Return: number of elemnts in lst
-  */
-size_t listint_len(const listint_t *h)
-{
-	/* i  is coiunter var */
-	int i;
-
-	i = 0;
-	while (h != NULL)
-	{
-		h = h->next;
-		i++;
-	}
-	return (i);
-}
 
 /**
-  * get_nodeint_at_index - returns Nth elemnt of linked list
-  * @head: pointer to firts element
-  * @index: node'sindex
-  *
-  * Return: Nth node otherwise NULL.
-  */
+ * get_nodeint_at_index - returns  nth node
+ * @head: head
+ * @index: index
+ * Return: nth node or NULL
+ */
+
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *current;
-	unsigned int count;
+	unsigned int count = 0;
 
-	current = head;
-	count = listint(head);
-
-	if (current == NULL)
-		count++;
-
-	if (count <= index)
+	if (head == NULL)
 	{
 		return (NULL);
 	}
-	else
+	while (count < index)
 	{
-		count = 0;
-		while (current != NULL)
+		head = head->next;
+		count++;
+		if (head == NULL)
 		{
-			if (count == index)
-			{
-				break;
-			}
-			else
-			{
-				count++;
-				current = current->next;
-			}
+			return (NULL);
 		}
-		return (current);
 	}
+	return (head);
 }
